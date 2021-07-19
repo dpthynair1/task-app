7263a01f6415ab8d8c348ccb6547120a67843064
Deepthy Nair <dpthynair@gmail.com>
2021-07-19 22:35:42 +0200

    Integrated with json server

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 import '/Users/dpthynair/React/task-app/src/style.css';
 import Button from './Button.component';
-
+import {useLocation} from 'react-router-dom'
 
 const Header = ({title, onAdd,showAdd}) =>{
 
-
+const Location = useLocation()
    
     return <header>
 
     <h1>{title}</h1>
-    <Button  color={showAdd ? 'red' : "green"} text={showAdd? 'close': 'Add'} onClick= {onAdd}/>
+    {Location.pathname === '/' &&<Button  color={showAdd ? 'red' : "green"} text={showAdd? 'close': 'Add'} onClick= {onAdd}/>}
     </header>
 }
 
